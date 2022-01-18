2006a72907bdcb5edb834acdeb221363d8a859e3
Lawrence Y <84044148+lawrenceyim@users.noreply.github.com>
2022-01-18 11:33:49 -0500

    Add: Leetcode Java Solutions

--- 605_Can_Place_Flowers.java
+++ 605_Can_Place_Flowers.java
@@ -1,3 +1,4 @@
+// First solution using try catch blocks to bypass array out of bounds error
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int plantable = 0;
@@ -15,6 +16,26 @@ class Solution {
                     flowerbed[i] = 1;
                     plantable++;
                 }
+            }
+        }
+        return (plantable >= n);
+    }
+}
+
+// Refactored solution that removed unnecessary boolean variables
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int plantable = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                try {
+                    if (flowerbed[i - 1] == 1) continue;
+                } catch (Exception e) { }
+                try {
+                    if (flowerbed[i + 1] == 1) continue;
+                } catch (Exception e) { }
+                flowerbed[i] = 1;
+                plantable++;
             }
         }
         return (plantable >= n);
